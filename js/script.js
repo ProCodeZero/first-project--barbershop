724d4fe71c1df538760d0fd189996e1daddc7d31
ProCodeZero <denis.osipov.2288.02@gmail.com>
2023-08-23 20:29:31 +0300

    23.08.2023 last push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new Swiper('.gallery__swiper', {
+	navigator: {
+		nextEl: '.swiper-button-next',
+		pervEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}
+})
